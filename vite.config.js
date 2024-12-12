011ac014026b9a085a7173b8d1db481163dfd18a
FirstChoice112 <johan.skoog.1986@gmail.com>
2024-12-12 23:48:44 +0100

    coverage added

--- vite.config.js
+++ vite.config.js
@@ -7,5 +7,10 @@ export default defineConfig({
     setupFiles: "./setupTests.js",
     silent: true,
     reporters: ["default", "verbose"],
+    coverage: {
+      provider: "c8",
+      reporter: ["text", "lcov"],
+      all: true,
+    },
   },
 });
